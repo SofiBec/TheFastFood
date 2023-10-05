1de353ccde86ff8bdb11f7f70213c9efd08f7736
SofiBec <sofibechardo@gmail.com>
2023-10-05 13:22:51 -0300

    Cambios en el archivo footer_view.php

--- primer_proyecto/app/Views/front/footer_view.php
+++ primer_proyecto/app/Views/front/footer_view.php
@@ -1,6 +1,6 @@
 	<footer class="footer">
 		<div class="footer-content">
-			<p class="text-center", style="background-color:#636363; color: #FFFFFF;">The Fast Food 2023. Copyright ©</p>
+			<p class="text-center", style="background-color:#636363; color: #FFFFFF;">Todos los derechos registrados. The Fast Food 2023. Copyright ©</p>
 		</div>
 	</footer>
 		<script src="assets/js/bootstrap.bundle.min.js"></script>
